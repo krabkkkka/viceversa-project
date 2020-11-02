55daa28203c2e0ee9ff6621add3e0654de40bcf1
krabka <www.andrkrab@mail.ru>
2020-11-02 10:56:48 +0300

    Add words count feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -9,5 +9,6 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
+	len_words = len(user_text.split())
 	return render(request, 'reverse.html', {'usertext': user_text, 
-		'reversedtext':reversed_text})
+		'reversedtext':reversed_text, 'lenwords':len_words})
